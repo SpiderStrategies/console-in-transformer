a3f029aa99c6776f00909d06834674b012634181
Dan Kolz <dpkolz@gmail.com>
2016-07-29 18:05:04 -0500

    more natural handling of keyboard input

--- console-in-transformer.js
+++ console-in-transformer.js
@@ -1,5 +1,13 @@
 var fs = require('fs')
 
+var isTTY = process.stdin.isTTY
+
+function printMsgIfNeeded() {
+	if(isTTY) {
+		console.log('Type or paste the text to be quoted. Press CTRL-D when done.')
+	}
+}
+
 var processInput = function(transform, additionalDataTerminator) {
 	additionalDataTerminator = additionalDataTerminator || ''
 	if(process.argv.length > 2) {
@@ -16,6 +24,7 @@ var processInput = function(transform, additionalDataTerminator) {
 		// No arguments to the command. Let's assume they want to process stdin
 		// This has the nice effect allowing content either piped in or allowing
 		// the user to paste in content (ctrl-d terminates).
+		printMsgIfNeeded()
 		processStdIn(transform)
 	}
 
@@ -28,9 +37,22 @@ var processInput = function(transform, additionalDataTerminator) {
 			if (chunk !== null) {
 				data += chunk
 			}
+			if(isTTY && chunk && chunk.length > 0) {
+				var i = chunk.charCodeAt(chunk.length -1)
+				if(i !== 10) {
+					// If we got readable data without the last char being a
+					// return then somebody pressed a break key.
+					process.stdin.emit('end')
+				}
+			}
 		})
 
 		process.stdin.on('end', () => {
+			if(isTTY) {
+				// if we're typing in a terminal we probably need a marker to tell
+				// when the first of the quoted text is being output
+				console.log('\nTransformed ->')
+			}
 			process.stdout.write(transform(data) + additionalDataTerminator);
 		})
 	}
